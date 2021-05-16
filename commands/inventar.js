63a1ecd97358eefc6af73cdb5179bbcf40847d93
zitronenclara <clara.hertel@outlook.de>
2021-05-16 16:53:43 +0200

    v1.3-alpha inventory command improved (WIP)

--- commands/inventar.js
+++ commands/inventar.js
@@ -1,34 +1,60 @@
 const Discord = require('discord.js')
 const misc = require('./../functions/misc.js')
 
-const sub = [
-    {
-        "name": "items",
-        "execute": require('./subcommands/inventar_items.js')
-    }
-]
+const sub = [{
+    "name": "items",
+    "execute": require('./subcommands/inventar_items.js')
+}]
 
 module.exports = {
     name: 'inventar',
     description: 'Verschiedene Befehle zur Verwaltung deines Inventars',
     options: [
         {
-            "name": "items",
-            "description": "Zeigt dir alle Items die du aktuell in deinem Inventar hast",
-            "type": 1,
-            "options": []
-        },
-        {
             "name": "einsetzen",
             "description": "Wenn du ein Item deines Inventars einsetzen möchtest",
             "type": 1,
-            "options": [{
-                "name": "ID",
-                "description": "Die ID des Items, kann mit /inventar items abgerufen werden",
-                "type": 4,
-                "required": true
-            }]
-        }
+            "options": [
+                {
+                    "name": "id",
+                    "description": "Die ID des Items, kann mit /inventar items abgerufen werden",
+                    "type": 4,
+                    "required": true
+                }
+            ]
+        },
+        {
+            "name": "items",
+            "description": "Zeigt dir alle Items die du aktuell in deinem Inventar hast",
+            "type": 1,
+            "options": [
+                {
+                    "name": "seite",
+                    "description": "Die Seite des Inventars, die du abrufen möchtest, beginnt bei 1",
+                    "type": 4,
+                    "required": false
+                },
+                {
+                    "name": "user",
+                    "description": "Wenn du das Inventar eines anderen Users ansteuern willst",
+                    "type": 6,
+                    "required": false
+                }
+            ]
+        },
+        {
+            "name": "iteminfo",
+            "description": "Wenn du mehr Infos über ein bestimmtes Item in deinem Inventar erhalten möchtest",
+            "type": 1,
+            "options": [
+                {
+                    "name": "id",
+                    "description": "Die ID des Items, kann mit /inventar items abgerufen werden",
+                    "type": 4,
+                    "required": true
+                }
+            ]
+        },
     ],
     async execute(cP) {
         let subCommand = sub.find(s => s.name === cP.args[0].name)
--- commands/subcommands/inventar_items.js
+++ commands/subcommands/inventar_items.js
@@ -1,3 +1,26 @@
+const botUser = require('./../../modules/userdataHandler.js')
+
 module.exports = async function(cP) {
-    
+    let args = cP.args[0].options
+    let targetArg = args?.find(t => t.name === "user")?.value
+    let pageArg = args?.find(t => t.name === "seite")?.value
+
+    let target;
+    let page;
+    if (args === undefined){
+        target = cP.author.data
+        page = 1
+    }else{
+        if (!targetArg){
+            target = cP.author.data
+        }else{
+            target = await botUser.load(cP.client, targetArg)
+        }
+
+        if (!pageArg){
+            page = 1
+        }else{
+            page = pageArg
+        }
+    }
 }
