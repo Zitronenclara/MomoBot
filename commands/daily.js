40e8509a7b2e131fb2c06322c0a758aef7208895
zitronenclara <clara.hertel@outlook.de>
2021-05-19 18:57:26 +0200

    v1.3-alpha daily command init

---
+++ commands/daily.js
@@ -0,0 +1,10 @@
+const misc = require('./../functions/misc.js')
+
+module.exports = {
+	name: 'daily',
+	description: 'Hiermit kannst du deine täglichen MomoCoins abholen!',
+	options: [],
+	async execute(cP) {
+		
+	}
+};
