72e67346324b86763ea21ad96269f6bb8b227384
zitronenclara <clara.hertel@outlook.de>
2021-05-20 21:42:14 +0200

    v1.3-alpha added more booster items to booster shop

--- inventory/shop/xpboosters.js
+++ inventory/shop/xpboosters.js
@@ -15,11 +15,35 @@ module.exports = {
         },
         {
             item: new XpMultiplicator(1, {duration: 1800000, factor: 1.5}),
-            price: 350
+            price: 400
         },
         {
             item: new XpMultiplicator(1, {duration: 3600000, factor: 1.5}),
             price: 600
+        },
+        {
+            item: new XpMultiplicator(1, {duration: 1800000, factor: 2}),
+            price: 800
+        },
+        {
+            item: new XpMultiplicator(1, {duration: 3600000, factor: 2}),
+            price: 1200
+        },
+        {
+            item: new XpMultiplicator(1, {duration: 1800000, factor: 3}),
+            price: 1500
+        },
+        {
+            item: new XpMultiplicator(1, {duration: 3600000, factor: 3}),
+            price: 2400
+        },
+        {
+            item: new XpMultiplicator(1, {duration: 1800000, factor: 5}),
+            price: 3000
+        },
+        {
+            item: new XpMultiplicator(1, {duration: 3600000, factor: 5}),
+            price: 4800
         }
     ]
 }
