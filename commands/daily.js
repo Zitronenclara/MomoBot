715adc2b6a060196f6c5bc9e8203679012e0d3b4
zitronenclara <clara.hertel@outlook.de>
2021-05-20 13:44:00 +0200

    v1.3-alpha daily command text adjusted

--- commands/daily.js
+++ commands/daily.js
@@ -12,10 +12,10 @@ module.exports = {
 		let dailyStreak = cP.author.data.economyData.dailyStreak
 		let maxStreak = cP.author.data.economyData.maxStreak
 		if (!daily.success){
-			return await misc.generateEmbed(cP.client, "⚠️ Fehler ⚠️", daily.message+"\n\n`Tägliche Belohnung:` **`"+dailyGain+" MomoCoins`**\n`aktuelle Daily-Streak:` **`"+dailyStreak+" (+"+(dailyStreak*2.5)+"% tägliche MomoCoins)`**\n`maximale Daily-Streak` **`"+maxStreak+"`**", cP.author, "0xf52411", cP.interaction)
+			return await misc.generateEmbed(cP.client, "⚠️ Fehler ⚠️", daily.message+"\n\n`Tägliche Belohnung:` **`"+dailyGain+" MomoCoins`**\n`aktuelle Daily-Streak:` **`"+dailyStreak+" (+"+(dailyStreak*2.5)+"% tägliche MomoCoins)`**\n`maximale Daily-Streak:` **`"+maxStreak+"`**", cP.author, "0xf52411", cP.interaction)
 		}else{
 			await cP.author.data.save().catch(err => console.log(err));
-			return await misc.generateEmbed(cP.client, "✅ Tägliche MomoCoins abgeholt ✅", "Du hast **`"+daily.amount+" MomoCoins`** erhalten c:"+"\n\n`Tägliche Belohnung:` **`"+dailyGain+" MomoCoins`**\n`aktuelle Daily-Streak:` **`"+dailyStreak+" (+"+(dailyStreak*2.5)+"% tägliche MomoCoins)`**\n`maximale Daily-Streak` **`"+maxStreak+"`**", cP.author, "0xe1ff00", cP.interaction)
+			return await misc.generateEmbed(cP.client, "✅ Tägliche MomoCoins abgeholt ✅", "Du hast **`"+daily.amount+" MomoCoins`** erhalten c:"+"\n\n`Tägliche Belohnung:` **`"+dailyGain+" MomoCoins`**\n`aktuelle Daily-Streak:` **`"+dailyStreak+" (+"+(dailyStreak*2.5)+"% tägliche MomoCoins)`**\n`maximale Daily-Streak:` **`"+maxStreak+"`**", cP.author, "0xe1ff00", cP.interaction)
 		}
 	}
 };
